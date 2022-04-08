c674493336574e06277e6dfcbb4df6e9d904e8cd
Nikita <nikita.krymov@bk.ru>
2022-04-08 19:55:29 +0300

    change

--- keyboards/client_kb.py
+++ keyboards/client_kb.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 b1 = KeyboardButton('/Режим_работы')
 b2 = KeyboardButton('/Расположение')
@@ -6,4 +6,4 @@ b3 = KeyboardButton('/Меню')
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
-kb_client.add(b1).add(b2).add(b3)
+kb_client.insert(b1).insert(b2).insert(b3)
